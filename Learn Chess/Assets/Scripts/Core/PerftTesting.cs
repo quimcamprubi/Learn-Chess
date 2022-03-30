35ae4a657d58cc584f2689d411446f117612f498
Quim Camprubí Casas <quimcc08@gmail.com>
2022-03-31 00:09:50 +0200

    Implemented main Perft function

---
+++ Learn Chess/Assets/Scripts/Core/PerftTesting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Core {
+    public static class PerftTesting {
+        public static int PerftCount(Board board, int depth) {
+            int nodes = 0;
+            board.CheckBoard();
+            if (depth == 0) {
+                return 1;
+            }
+            List<Move> moveList = MoveGenerator.GenerateAllMoves(board);
+            foreach (Move move in moveList) {
+                if (!board.MakeMove(move)) { // If move is illegal, do not explore any further
+                    continue;
+                }
+                nodes += PerftCount(board, depth - 1);
+                board.UnmakeMove();
+            }
+            return nodes;
+        }
+        
+        public static int PerftTest(Board board, int depth) {
+            int nodes = 0;
+            board.CheckBoard();
+            board.PrintGameBoard();
+            
+            if (depth == 0) {
+                return 1;
+            }
+            List<Move> moveList = MoveGenerator.GenerateAllMoves(board);
+            foreach (Move move in moveList) {
+                if (!board.MakeMove(move)) { // If move is illegal, do not explore any further
+                    continue;
+                }
+                nodes += PerftCount(board, depth - 1);
+                board.UnmakeMove();
+            }
+            return nodes;
+        }
+    }
+}
